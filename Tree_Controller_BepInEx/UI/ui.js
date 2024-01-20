92798de9d21c275df0153d4388ddc91e604c6a59
yenyang <tjkelley19@gmail.com>
2024-01-19 19:42:03 -0800

    Revert "trying to get locale keys to work"
    This reverts commit 48d57c06d56c5895936bbe9cc8cb74f84491c5d3.

--- Tree_Controller_BepInEx/UI/ui.js
+++ Tree_Controller_BepInEx/UI/ui.js
@@ -33,7 +33,7 @@ if (typeof yyTreeController.buildTreeAgeItem !== 'function') {
         const changeAgeRowLabel = document.createElement("div");
         changeAgeRowLabel.id = "YYTC-change-age-label";
         changeAgeRowLabel.className = "label_RZX";
-        changeAgeRowLabel.localeKey = "YY_TREE_CONTROLLER.Age";
+        changeAgeRowLabel.innerHTML = "Age";
         const changeAgeButtonsPanel = document.createElement("div");
         changeAgeButtonsPanel.className = "content_ZIz";
         changeAgeButtonsPanel.id = "YYTC-change-age-buttons-panel";
@@ -209,7 +209,7 @@ if (typeof yyTreeController.buildRadiusChanger !== 'function') {
         const radiusRowLabel = document.createElement("div");
         radiusRowLabel.id = "YYTC-radius-row-label";
         radiusRowLabel.className = "label_RZX";
-        radiusRowLabel.localeKey = "YY_TREE_CONTROLLER.Radius";
+        radiusRowLabel.localeKey = "Radius";
         const radiusButtonsPanel = document.createElement("div");
         radiusButtonsPanel.className = "content_ZIz";
         radiusButtonsPanel.id = "YYTC-radius-buttons-panel";
@@ -323,7 +323,7 @@ if (typeof yyTreeController.buildPrefabSetsRow !== 'function') {
         const prefabSetsRowLabel = document.createElement("div");
         prefabSetsRowLabel.id = "YYTC-prefab-label";
         prefabSetsRowLabel.className = "label_RZX";
-        prefabSetsRowLabel.localeKey = "YY_TREE_CONTROLLER.Sets";
+        prefabSetsRowLabel.localeKey = "Sets";
         const prefabSetsButtonsPanel = document.createElement("div");
         prefabSetsButtonsPanel.className = "content_ZIz";
         prefabSetsButtonsPanel.id = "YYTC-prefab-sets-buttons-panel";
@@ -440,7 +440,7 @@ if (typeof yyTreeController.buildRotationRow !== 'function') {
         }
 
         yyTreeController.setTooltip(button.id, "random-rotation");
-        yyTreeController.applyLocalization(rotationRowLabel);
+        yyTreeController.applyLocalization(rotationRow);
     }
 }
 
